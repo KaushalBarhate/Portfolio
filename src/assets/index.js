d13ba0417141576c27affe2d07929c00f920b5f9
KaushalBarhate <79403580+KaushalBarhate@users.noreply.github.com>
2023-08-12 18:31:33 +0530

    src

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,15 +1,17 @@
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
+import internet from "./internet.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
+import firebase from "./tech/firebase.png";
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
+import python from "./tech/python.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
@@ -21,14 +23,12 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import bosch from "./company/bosch.jpeg";
+import bhai from "./company/bhai.jpeg";
+import surya from "./company/surya.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 
 export {
   logo,
@@ -52,11 +52,12 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  bosch,
+  bhai,
+  surya,
   carrent,
   jobit,
-  tripguide,
+  internet,
+  python,
+  firebase
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -15,14 +15,12 @@ import {
   git,
   figma,
   docker,
-  meta,
-  starbucks,
-  tesla,
-  shopify,
+ bosch,bhai,surya,
   carrent,
   jobit,
-  tripguide,
   threejs,
+  python,
+  firebase
 } from "../assets";
 
 export const navLinks = [
@@ -42,11 +40,11 @@ export const navLinks = [
 
 const services = [
   {
-    title: "Web Developer",
+    title: "Fullstack Developer",
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "ML Developer",
     icon: mobile,
   },
   {
@@ -54,12 +52,16 @@ const services = [
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Blockchain Developer",
     icon: creator,
   },
 ];
 
 const technologies = [
+  {
+    name: "Python",
+    icon: python,
+  },
   {
     name: "HTML 5",
     icon: html,
@@ -97,16 +99,12 @@ const technologies = [
     icon: mongodb,
   },
   {
-    name: "Three JS",
-    icon: threejs,
+    name: "Firebase",
+    icon: firebase,
   },
   {
     name: "git",
     icon: git,
-  },
-  {
-    name: "figma",
-    icon: figma,
   },
   {
     name: "docker",
@@ -116,55 +114,40 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
-    icon: starbucks,
-    iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    title: "Summer Intern",
+    company_name: "Bosch India",
+    icon: bosch, // You need to provide the appropriate icon
+    date: "May 2023 - June 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Developed a graph conversion tool using VBA to automate the plotting of graphs for millions of data points.",
+      "Implemented a comprehensive table generation feature with knee point and jump calculations for data analysis.",
+      "Streamlined iSTAR request tracking and reporting through Automation Anywhere, extracting data, processing it in Excel, generating reports, and emailing stakeholders.",
+      "Worked on the Zeegma project to digitalize checklists using JavaScript.",
     ],
   },
   {
-    title: "React Native Developer",
-    company_name: "Tesla",
-    icon: tesla,
-    iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    title: "Software Engineer Intern",
+    company_name: "Backend Helping AI",
+    icon: bhai, // You need to provide the appropriate icon
+    date: "Jan 2022 – May 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Led the development of a web3 game marketplace for a US-based client using React, Redux, and Sanity, allowing users to buy games with crypto, featuring separate game pages, bookmarks, and a unique carousel for featured games.",
+      "Developed decentralized application with real-time graph database and express-based relay nodes, utilizing Svelte for frontend and svelte-web3 for Metamask integration and NFT retrieval.",
+      "Spearheaded an experimental project called Lipi, a smart contract deployer for Ethereum/Polygon mainnet and testnet.",
+      "Implemented Family Tree NFT generator with Nextjs and breadth-first search algorithm to store tree structure, using Firebase to convert generated tree images to NFTs.",
+      "Integrated Tilt Five SDK in Unity to develop an augmented reality application for immersive viewing of AI-generated images using C#.",
+      "Web scraped 40-50k images with icrawler and filtered out similar images with difOy package using data mining and machine learning techniques.",
     ],
   },
   {
-    title: "Web Developer",
-    company_name: "Shopify",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
+    title: "Web Developer Intern",
+    company_name: "Surya Nestbuild Limited",
+    icon: surya, // You need to provide the appropriate icon
+    date: "May 2021 – July 2021",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Developed a dynamic and user-friendly website to showcase all construction projects, including news and blog articles relevant to the company.",
+      "Created a secure admin page to allow existing staff to easily edit projects, blogs, and news articles on the website.",
+      "Developed a super admin page to generate and revoke login credentials for the admin page, providing additional security and control over website content management.",
     ],
   },
 ];
@@ -198,68 +181,91 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "AiBlocks",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Developed a web-based AI image generator that utilizes stable diffusion models to generate unique and artistic images. Implemented a credit system and token gating system for exclusive model access.",
     tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "mongodb",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "pink-text-gradient",
-      },
+      { name: "Reactjs", color: "blue-text-gradient" },
+      { name: "Chakra UI", color: "green-text-gradient" },
+      { name: "Firebase", color: "pink-text-gradient" },
+      { name: "Web3", color: "blue-text-gradient" },
+      { name: "Deep Learning", color: "green-text-gradient" },
+      // Add other technologies if needed
     ],
-    image: carrent,
-    source_code_link: "https://github.com/",
+    image: "./demo/aiblocksdemo.png", // Replace with the correct variable or URL
+    source_code_link: "https://aiblocks.app",
   },
   {
-    name: "Job IT",
+    name: "BlockSafe",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      "A Web3 app built in 48 hours during the HackKnight Hackathon to minimize fraudulent certificates by deploying them as NFTs in the Blockchain.",
     tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "restapi",
-        color: "green-text-gradient",
-      },
-      {
-        name: "scss",
-        color: "pink-text-gradient",
-      },
+      { name: "NodeJS", color: "blue-text-gradient" },
+      { name: "ExpressJS", color: "green-text-gradient" },
+      { name: "Solidity", color: "pink-text-gradient" },
     ],
-    image: jobit,
-    source_code_link: "https://github.com/",
+    image: "./demo/blocksafedemo.png", // Replace with the correct variable or URL
+    source_code_link: "https://github.com/KaushalBarhate/BlockSafe", // Update with correct link
   },
   {
-    name: "Trip Guide",
+    name: "EcoScript Programming Language",
     description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      "A pioneering programming language designed with sustainability in mind. Supports arithmetic operations, control flow, and calculates energy consumption and carbon footprint.",
     tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
+      { name: "Python", color: "blue-text-gradient" },
     ],
-    image: tripguide,
-    source_code_link: "https://github.com/",
+    image: "./demo/ecoscriptdemo.png", // Replace with the correct variable or URL
+    source_code_link: "https://ecoscript.vercel.app/",
   },
-];
+  {
+    name: "Chalkmomentlogger",
+    description:
+      "A lightweight Node.js module that provides easy logging functionalities, allowing output to console and optional save to a log file.",
+    tags: [
+      { name: "Node.js", color: "blue-text-gradient" },
+      // Add other technologies if needed
+    ],
+    image: "./demo/chalkmomentdemo.png", // Replace with the correct variable or URL
+    source_code_link: "https://www.npmjs.com/package/chalkmomentlogger",
+  },
+  {
+    name: "Mstar-sar",
+    description:
+      "Achieved maximum accuracy for mstar dataset using 5 deep learning pre-trained models.",
+    tags: [
+      { name: "Python", color: "blue-text-gradient" },
+      { name: "Deep Learning", color: "green-text-gradient" },
+    ],
+    image: "./demo/mstardemo.jpg", 
+    source_code_link: "https://github.com/KaushalBarhate/MStar-SAR",
+  },
+  {
+    name: "Family Tree",
+    description:
+      "NFT generator with Nextjs and a breadth-first search algorithm to store tree structure, using Firebase to convert generated tree images to NFTs.",
+    tags: [
+      { name: "Nextjs", color: "blue-text-gradient" },
+      { name: "Firebase", color: "green-text-gradient" },
+      { name: "TypeScript", color: "pink-text-gradient" },
+      
+    ],
+    image: "./demo/familytreedemo.png", // Replace with the correct variable or URL
+    source_code_link: "https://github.com/KaushalBarhate/Family-Tree-NFT",
+  },
+  {
+    name: "TeamsOCR",
+    description:
+      "Python project to automatically capture screenshots of Microsoft Teams meetings, crop images with OpenCV, and extract text with pytesseract for OCR.",
+    tags: [
+      { name: "Python", color: "blue-text-gradient" },
+      { name: "OpenCV", color: "green-text-gradient" },
+      { name: "OCR", color: "pink-text-gradient" },
+    ],
+    image: "./demo/teamsocrdemo.png", // Replace with the correct variable or URL
+    source_code_link: "https://github.com/KaushalBarhate/TeamsOCR",
+  }
+  
+ ];
+
 
 export { services, technologies, experiences, testimonials, projects };
