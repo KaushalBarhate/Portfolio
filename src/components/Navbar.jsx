50b0896ba6ed062bddce3c6bb46236b4ed78203d
KaushalBarhate <79403580+KaushalBarhate@users.noreply.github.com>
2023-08-13 01:58:19 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -91,7 +91,11 @@ const Navbar = () => {
                     setActive(nav.title);
                   }}
                 >
-                  <a href={`#${nav.id}`}>{nav.title}</a>
+                   {nav.id=='resume'?
+              <a href={nav.drivelink} target="__blank">{nav.title}</a>
+              :
+              <a href={`#${nav.id}`}>{nav.title}</a>
+              }
                 </li>
               ))}
             </ul>
